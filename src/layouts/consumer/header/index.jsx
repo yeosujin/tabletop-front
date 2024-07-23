e6b3293cc75a468f022a964c8b31690dc16d20ee
gntodtndls156 <gntodtndls156@gmail.com>
2024-07-23 15:17:27 +0900

    feat: add info icon

--- src/layouts/consumer/header/index.jsx
+++ src/layouts/consumer/header/index.jsx
@@ -1,5 +1,7 @@
 import Grid from '@mui/material/Unstable_Grid2'
 import { ConsumerHeaderBox, ConsumerHeaderTitle } from './styled'
+import { InfoOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 
 const ConsumerHeader = ({ store, tableNo }) => {
     return (
@@ -7,6 +9,9 @@ const ConsumerHeader = ({ store, tableNo }) => {
             <Grid>
                 <ConsumerHeaderBox>
                     <ConsumerHeaderTitle>{store.name}</ConsumerHeaderTitle>
+                    <IconButton>
+                        <InfoOutlined />
+                    </IconButton>
                 </ConsumerHeaderBox>
             </Grid>
             <Grid>{/* menu */}</Grid>
