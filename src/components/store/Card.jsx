e783d87726a0cf13de4d02c222e434e727ee5a47
HYH <dihexyldecyl@gmail.com>
2024-07-26 10:26:59 +0900

    #fix: conflict 오류 해결

--- src/components/store/Card.jsx
+++ src/components/store/Card.jsx
@@ -8,74 +8,65 @@ import { Chip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Card = ({ store }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const navigate = useNavigate();
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
-  const moveToModifyStore = (storeId) => {
-    navigate('/modifystore', { state: { storeId } });
-  };
+    const moveToModifyStore = (storeId) => {
+      navigate('/modifystore', { state: { storeId } });
+    };
 
-  const moveToEditMenu = () => {
-    navigate('/menu', { state: { storeId: store.id } });
-  };
+    const moveToEditMenu = () => {
+      navigate('/menu', { state: { storeId: store.id } });
+    };
 
-  return (
-    <div className="card">
-        <Chip label={store.type} color="primary" />
-        <div>
-            <IconButton 
-              aria-label="menu"
-              id="fade-button"
-              aria-controls={open ? 'fade-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Menu
-              id="fade-menu"
-              MenuListProps={{
-                'aria-labelledby': 'fade-button',
-              }}
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              TransitionComponent={Fade}
-            >
-<<<<<<< Updated upstream
-              <MenuItem onClick={() => {
-                handleClose();
-                moveToEditMenu();
-              }}>Edit Menu</MenuItem>
-              <MenuItem onClick={() => {
-                handleClose();
-                moveToModifyStore(store.id);
-              }}>Modify Store</MenuItem>
-              <MenuItem onClick={handleClose}>Delete Store</MenuItem>
-=======
-              <MenuItem onClick={handleClose}>메뉴 수정</MenuItem>
-              <MenuItem onClick={() => {
-                handleClose();
-                moveToModifyStore(13);
-              }}>가게 정보 수정</MenuItem>
-              <MenuItem onClick={handleClose}>가게 삭제</MenuItem>
->>>>>>> Stashed changes
-            </Menu>
+    return (
+        <div className="card">
+            <Chip label={store.type} color="primary" />
+            <div>
+                <IconButton 
+                  aria-label="menu"
+                  id="fade-button"
+                  aria-controls={open ? 'fade-menu' : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? 'true' : undefined}
+                  onClick={handleClick}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="fade-menu"
+                  MenuListProps={{
+                    'aria-labelledby': 'fade-button',
+                  }}
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  TransitionComponent={Fade}
+                >
+                  <MenuItem onClick={() => {
+                    handleClose();
+                    moveToEditMenu();
+                  }}>메뉴 수정</MenuItem>
+                  <MenuItem onClick={() => {
+                    handleClose();
+                    moveToModifyStore(13);
+                  }}>가게 정보 수정</MenuItem>
+                  <MenuItem onClick={handleClose}>가게 삭제</MenuItem>
+                </Menu>
+            </div>
+            <p>{store.image}</p>
+            <h2>{store.name}</h2>
         </div>
-        <p>{store.image}</p>
-        <h2>{store.name}</h2>
-    </div>
-  );
+    );
 };
 
 export default Card;
