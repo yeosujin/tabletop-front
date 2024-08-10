207af334790f48d454ed82e41fcbb73d0bbb1395
gntodtndls156 <gntodtndls156@gmail.com>
2024-08-10 22:41:58 +0900

    feat: 결제 완료 후 리디렉션

--- src/pages/consumer/payment/index.jsx
+++ src/pages/consumer/payment/index.jsx
@@ -92,7 +92,7 @@ const PaymentPage = () => {
                     buyer_tel: '010-1234-5678',
                     buyer_addr: '서울특별시 강남구 삼성동',
                     buyer_postcode: '123-456',
-                    m_redirect_url: '{모바일에서 결제 완료 후 리디렉션 될 URL}',
+                    m_redirect_url: `${process.env.REACT_APP_API_URL}/consumer/1/complete`,
                     escrow: true,
                     vbank_due: 'YYYYMMDD',
                     bypass: {
